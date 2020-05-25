4d48e87a6bedf7a4d2200d3020938608e86d295b
juan miguel <juanmiguel@juans-iMac-6.local>
2020-05-25 20:27:35 -0300

    gitignore

--- clases/conexion.php
+++ clases/conexion.php
@@ -11,7 +11,7 @@ class claseConexion
     {
         $this->servidor = 'localhost';
         $this->usuario = 'estonoesunaweb_CDC';
-        $this->clave = '';
+        $this->clave = '123';
         $this->sql = 0;
     }
     
